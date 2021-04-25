90e06ffbb95b0e3942334ebb95f19f6f89ea11aa
Peter, Elvis <Elvis.Peter@bmo.com>
2021-04-25 19:54:05 -0400

    adding class methods to the track class

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,20 @@
 class Track < ApplicationRecord
-  belongs_to :album
-  belongs_to :genre
-  belongs_to :media_type
-  has_and_belongs_to_many :playlists
+  # belongs_to :album
+  # belongs_to :genre
+  # belongs_to :media_type
+  # has_and_belongs_to_many :playlists
+  SHORT = 180000
+  LONG = 360000
+  def self.short
+    where("milliseconds < ?",SHORT)
+  end
+
+  def self.long
+    where("milliseconds >= ?",LONG)
+  end
+
+  def self.medium
+    where("milliseconds >= ?",SHORT).where("milliseconds < ?", LONG)
+    # where("milliseconds >= ? AND milliseconds < ?", 180000 , 360000)
+  end
 end
